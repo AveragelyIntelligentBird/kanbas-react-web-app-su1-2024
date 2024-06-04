20c938e042adc07156e83cf3f3a63a4104f5183a
AveragelyIntelligentBird <anfisabogdanenko@gmail.com>
2024-06-04 02:13:45 -0400

    Almost completes the Kanbas implementation

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,12 +1,49 @@
-import { Link } from "react-router-dom";
-import * as db from "../Database";
-import {FaBookOpen} from "react-icons/fa6";
+import {Link} from "react-router-dom";
+import {FaBookOpen, FaPencil, FaTrashCan} from "react-icons/fa6";
 
-export default function Dashboard() {
-    const courses = db.courses;
+export default function Dashboard({courses, course, setCourse, addNewCourse, deleteCourse, updateCourse}: {
+    courses: any[];
+    course: any;
+    setCourse: (course: any) => void;
+    addNewCourse: () => void;
+    deleteCourse: (courseId: string) => void;
+    updateCourse: () => void;
+}) {
     return (
         <div id="wd-dashboard" className="px-5">
             <h1 id="wd-dashboard-title"><b>Dashboard</b></h1>
+            <hr/>
+            <h5 id="wd-new-course-controls">
+                New Course
+                <button
+                    className="btn btn-primary float-end"
+                    id="wd-add-new-course-click"
+                    onClick={addNewCourse}
+                >
+                    {" "}
+                    Add{" "}
+                </button>
+                <button
+                    className="btn btn-warning float-end me-2"
+                    onClick={updateCourse}
+                    id="wd-update-course-click"
+                >
+                    Update
+                </button>
+            </h5>
+            <br/>
+            <input id="wd-new-course-name"
+                   value={course.name}
+                   className="form-control mb-2"
+                   onChange={(e) =>
+                       setCourse({...course, name: e.target.value})}
+            />
+            <textarea id="wd-new-course-description"
+                      value={course.description}
+                      className="form-control"
+                      onChange={(e) =>
+                          setCourse({...course, description: e.target.value})}
+            />
             <hr/>
             <h3 id="wd-dashboard-published" className="mb-0">Published Courses ({courses.length})</h3>
             <hr className="mb-0"/>
@@ -33,10 +70,30 @@ export default function Dashboard() {
                                         {course.description}
                                     </p>
                                     <Link to={`/Kanbas/Courses/${course._id}/Assignments`}
-                                              className="text-decoration-none fs-5 p-1"
-                                              style={{color: "navy"}}>
-                                            <FaBookOpen/>
+                                          className="text-decoration-none fs-5 p-1"
+                                          style={{color: "navy"}}>
+                                        <FaBookOpen/>
                                     </Link>
+                                    <button
+                                        onClick={(event) => {
+                                            event.preventDefault();
+                                            deleteCourse(course._id);
+                                        }}
+                                        className="btn text-danger float-end fs-5 p-1"
+                                        id="wd-delete-course-click"
+                                    >
+                                        <FaTrashCan />
+                                    </button>
+                                    <button
+                                        id="wd-edit-course-click"
+                                        onClick={(event) => {
+                                            event.preventDefault();
+                                            setCourse(course);
+                                        }}
+                                        className="btn text-warning me-2 float-end fs-5 p-1"
+                                    >
+                                        <FaPencil/>
+                                    </button>
                                 </div>
                             </div>
                         </Link>
@@ -44,23 +101,6 @@ export default function Dashboard() {
                 ))}
             </div>
         </div>
-
-        // <div>
-        //     <h2 id="wd-dashboard-title">Dashboard</h2>
-        //     <hr/>
-        //     <div id="wd-dashboard-courses" className="row px-4">
-        //         <h4 id="wd-dashboard-published">Published Courses (7)</h4>
-        //         <hr/>
-        //         <div className="row row-cols-1 row-cols-md-5 g-4 my-0">
-        //             <OIAR101/>
-        //             <OIAR111/>
-        //             <OIAR201/>
-        //             <OIAR202/>
-        //             <OIAR211/>
-        //             <OIAR212/>
-        //             <OIAR311/>
-        //         </div>
-        //     </div>
-        // </div>
-    );
-}
+    )
+        ;
+};
