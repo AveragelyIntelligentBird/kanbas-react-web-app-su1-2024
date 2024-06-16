4182a9849848b46e78111b731a9730d12a50fce7
AveragelyIntelligentBird <anfisabogdanenko@gmail.com>
2024-06-16 12:32:27 -0400

    Completes A6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,14 +1,17 @@
 import { Navigate, Route, Routes } from "react-router";
 import Courses from "./Courses";
 import Dashboard from "./Dashboard";
+import Account from "./Account";
 import KanbasNavigation from "./Navigation";
 import "./styles.css";
 import {useEffect, useState} from "react";
 import * as client from "./Courses/client"
 import store from "./store";
 import { Provider } from "react-redux";
+import ProtectedRoute from "./Account/ProtectedRoute";
 
 export default function Kanbas() {
+    const [error, setError] = useState("");
     const [courses, setCourses] = useState<any[]>([]);
     const fetchCourses = async () => {
         const courses = await client.fetchAllCourses();
@@ -33,13 +36,19 @@ export default function Kanbas() {
             image: "/images/reactjs.jpg",
             number: "XXXX"
         })
-        const newCourse = await client.createCourse(course);
-        setCourses([...courses, newCourse]);
+        try {
+            const newCourse = await client.createCourse(course);
+            setCourses([...courses, newCourse]);
+            setError("");
+        } catch (err: any) {
+            setError(err.response.data.message);
+        }
     };
     const deleteCourse = async (courseId: string) => {
         await client.deleteCourse(courseId);
         setCourses(courses.filter(
             (c) => c._id !== courseId));
+        setError("");
     };
     const updateCourse = async () => {
         await client.updateCourse(course);
@@ -52,6 +61,7 @@ export default function Kanbas() {
                 }
             })
         );
+        setError("");
     };
     return (
         <Provider store={store}>
@@ -61,12 +71,14 @@ export default function Kanbas() {
                         <KanbasNavigation/>
                     </div>
                     <div className="flex-fill p-4 h-100">
+                        {error && <div className="alert alert-danger">{error}</div>}
                         <Routes>
                             <Route path="/" element={<Navigate to="Dashboard"/>}/>
-                            <Route path="Account" element={<h1>Account</h1>}/>
+                            <Route path="/Account/*" element={<Account />} />
                             <Route
                                 path="Dashboard"
                                 element={
+                                    <ProtectedRoute>
                                     <Dashboard
                                         courses={courses}
                                         course={course}
@@ -75,11 +87,12 @@ export default function Kanbas() {
                                         deleteCourse={deleteCourse}
                                         updateCourse={updateCourse}
                                     />
+                                    </ProtectedRoute>
                                 }
                             />
                             <Route
                                 path="Courses/:cid/*"
-                                element={<Courses courses={courses}/>}
+                                element={<ProtectedRoute> <Courses courses={courses}/> </ProtectedRoute>}
                             />
                             <Route path="Calendar" element={<h1>Calendar</h1>}/>
                             <Route path="Inbox" element={<h1>Inbox</h1>}/>
