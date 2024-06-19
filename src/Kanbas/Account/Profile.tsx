b221beff9f8c553c96f436a88b52feacfacdb78a
AveragelyIntelligentBird <anfisabogdanenko@gmail.com>
2024-06-18 23:10:48 -0400

    Completes A6 - Clarifies profile page x2

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -5,97 +5,107 @@ import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
 
 export default function Profile() {
-  const [profile, setProfile] = useState<any>({});
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const [profile, setProfile] = useState<any>({});
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const fetchProfile = async () => {
-    try {
-      const account = await client.profile();
-      setProfile(account);
-    } catch (err: any) {
-      navigate("/Kanbas/Account/Signin");
-    }
-  };
-  const signout = async () => {
-    await client.signout();
-    dispatch(setCurrentUser(null));
+    const fetchProfile = async () => {
+        try {
+            const account = await client.profile();
+            setProfile(account);
+        } catch (err: any) {
+            navigate("/Kanbas/Account/Signin");
+        }
+    };
+    const signout = async () => {
+        await client.signout();
+        dispatch(setCurrentUser(null));
 
-    navigate("/Kanbas/Account/Signin");
-  };
+        navigate("/Kanbas/Account/Signin");
+    };
 
-  useEffect(() => {
-    fetchProfile();
-  }, []);
-  return (
-    <div>
-      <h1>Profile</h1>
-      {profile && (
+    const updateProfile = async () => {
+        await client.updateProfile(profile)
+    };
+
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+    return (
         <div>
-          <input
-            value={profile.username}
-            onChange={(e) =>
-              setProfile({ ...profile, username: e.target.value })
-            }
-            className="form-control mb-2"
-            placeholder="username"
-            title="Username"
-          />
-          <input
-            value={profile.password}
-            onChange={(e) =>
-              setProfile({ ...profile, password: e.target.value })
-            }
-            className="form-control mb-2"
-            title="Password"
-          />
-          <input
-            value={profile.firstName}
-            onChange={(e) =>
-              setProfile({ ...profile, firstName: e.target.value })
-            }
-            className="form-control mb-2"
-            placeholder="John"
-            title="First Name"
-          />
-          <input
-            value={profile.lastName}
-            onChange={(e) =>
-              setProfile({ ...profile, lastName: e.target.value })
-            }
-            className="form-control mb-2"
-            placeholder="Doe"
-            title="Last Name"
-          />
-          <input
-            value={profile.dob}
-            onChange={(e) => setProfile({ ...profile, dob: e.target.value })}
-            type="date"
-            className="form-control mb-2"
-            title="Date of Birth"
-          />
-          <input
-            value={profile.email}
-            onChange={(e) => setProfile({ ...profile, email: e.target.value })}
-            className="form-control mb-2"
-            placeholder="example@email.com"
-            title="Email"
-          />
-          <select
-            onChange={(e) => setProfile({ ...profile, role: e.target.value })}
-            className="form-control mb-2"
-            title="Role"
-          >
-            <option value="USER">User</option>{" "}
-            <option value="ADMIN">Admin</option>
-            <option value="FACULTY">Faculty</option>{" "}
-            <option value="STUDENT">Student</option>
-          </select>
+            <h1>Profile</h1>
+            {profile && (
+                <div>
+                    <input
+                        value={profile.username}
+                        onChange={(e) =>
+                            setProfile({...profile, username: e.target.value})
+                        }
+                        className="form-control mb-2"
+                        placeholder="username"
+                        title="Username"
+                    />
+                    <input
+                        value={profile.password}
+                        onChange={(e) =>
+                            setProfile({...profile, password: e.target.value})
+                        }
+                        className="form-control mb-2"
+                        title="Password"
+                    />
+                    <input
+                        value={profile.firstName}
+                        onChange={(e) =>
+                            setProfile({...profile, firstName: e.target.value})
+                        }
+                        className="form-control mb-2"
+                        placeholder="John"
+                        title="First Name"
+                    />
+                    <input
+                        value={profile.lastName}
+                        onChange={(e) =>
+                            setProfile({...profile, lastName: e.target.value})
+                        }
+                        className="form-control mb-2"
+                        placeholder="Doe"
+                        title="Last Name"
+                    />
+                    <input
+                        value={profile.dob}
+                        onChange={(e) => setProfile({...profile, dob: e.target.value})}
+                        type="date"
+                        className="form-control mb-2"
+                        title="Date of Birth"
+                    />
+                    <input
+                        value={profile.email}
+                        onChange={(e) => setProfile({...profile, email: e.target.value})}
+                        className="form-control mb-2"
+                        placeholder="example@email.com"
+                        title="Email"
+                    />
+                    <select
+                        value={profile.role}
+                        onChange={(e) => setProfile({...profile, role: e.target.value})}
+                        className="form-control mb-2"
+                        title="Role"
+                    >
+                        <option value="USER">User</option>
+                        {" "}
+                        <option value="ADMIN">Admin</option>
+                        <option value="FACULTY">Faculty</option>
+                        {" "}
+                        <option value="STUDENT">Student</option>
+                    </select>
+                </div>
+            )}
+            <button onClick={updateProfile} className="float-start btn btn-danger w-100">
+                Update details
+            </button>
+            <button onClick={signout} className="float-start btn btn-danger w-100">
+                Sign out
+            </button>
         </div>
-      )}
-      <button onClick={signout} className="btn btn-danger w-100">
-        Sign out
-      </button>
-    </div>
-  );
-}
+    );
+};
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -4,6 +4,11 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
+export const updateProfile = async (user: any) => {
+  const response = await axios.put(`${USERS_API}/${user._id}`, user);
+  return response.data;
+};
+
 export const signin = async (credentials: any) => {
     const response = await axiosWithCredentials.post(
         `${USERS_API}/signin`,
@@ -11,6 +16,7 @@ export const signin = async (credentials: any) => {
     );
     return response.data;
 };
+
 
 export const profile = async () => {
     const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
